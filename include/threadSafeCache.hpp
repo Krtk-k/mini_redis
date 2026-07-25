6867e68cd00469c8d91abec20b7458ab72ed2b1b
Kartik <kartikkpandey05@gmail.com>
2026-07-25 22:43:50 +0530

    complete ThreadSafeCache

--- include/threadSafeCache.hpp
+++ include/threadSafeCache.hpp
@@ -13,12 +13,40 @@ struct Bucket {
 class ThreadSafeCache {
     vector<unique_ptr<Bucket>> cache;
     int bukets;
-    int add_calc(int )
+    int buck_idx(string &key) {
+        hash<string> hasher;
+        size_t num = hasher(key);
+        return num%this->buckets;
+    }
     public:
     ThreadSafeCache(int n) : buckets {
         for(int i = 0; i<buckets; i++) {
-            cahce.push_back(make_unique<Bucket>());
+            cache.push_back(make_unique<Bucket>());
         }
     }
-
+    string get(string key) {
+        int b_num = buck_idx(key);
+        unique_ptr<Bucket> curr = cache[b_num];
+        curr->lock.acq_read();
+        if(curr->mp.find(key) == curr->mp.end()) return "NULL";
+        else return curr->mp[key];
+        curr->lock.rel_read();
+    }
+    void set(string key, string val) {
+        int b_num = buck_idx(key);
+        int b_num = buck_idx(key);
+        unique_ptr<Bucket> curr = cache[b_num];
+        curr->lock.acq_write();
+        curr->mp[key] = val;
+        curr->lock.rel_write();
+    }
+    int get_size() {
+        size_t size = 0;
+        for(int i = 0; i<buckets; i++) {
+            cache[i]->lock.acq_write();
+            size += cache[i]->mp.size();
+            cache[i]->lock.rel_write();
+        }
+        return size;
+    }
 };
